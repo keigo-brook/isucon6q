4a05bfde6017428b42f3d2d9912d2d956e6e158a
Keigo Ogawa <goog@brookbach.com>
2016-09-18 16:22:46 +0900

    update

--- ruby/config/unicorn/isuda.rb
+++ ruby/config/unicorn/isuda.rb
@@ -1,6 +1,6 @@
-listen '/tmp/unicorn_isuda.sock'
-listen 5000
-pid "/home/isucon/webapp/ruby/unicorn_isuda.pid"
+#listen '/tmp/unicorn_isuda.sock', backlog: 8192
+#listen 5000
+#pid "/home/isucon/webapp/ruby/unicorn_isuda.pid"
 worker_processes 5
 preload_app true
 timeout 120
--- ruby/config/unicorn/isutar.rb
+++ ruby/config/unicorn/isutar.rb
@@ -1,6 +1,6 @@
-listen '/tmp/unicorn_isutar.sock'
-listen 5001
-pid "/home/isucon/webapp/ruby/unicorn_isutar.pid"
+#listen '/tmp/unicorn_isutar.sock', backlog: 8192
+#listen 5001
+#pid "/home/isucon/webapp/ruby/unicorn_isutar.pid"
 worker_processes 5
 preload_app true
 timeout 120
